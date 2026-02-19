839da2f7c3ad44229e1f84833910c25f0f1d5473
the-white <birkanbeyaz@hotmail.com>
2026-02-19 21:33:54 +0300

    Update locations.ts

--- data/locations.ts
+++ data/locations.ts
@@ -1,9 +1,14 @@
 export const provinces = [
+  { id: 06, name: "Ankara" },
   { id: 10, name: "Balıkesir" },
   { id: 34, name: "İstanbul" },
 ];
 
 export const districts: Record<number, { id: number; name: string }[]> = {
+   06: [
+    { id: 3401, name: "Eryaman" },
+    { id: 3402, name: "Balgat" },
+  ],
   10: [
     { id: 1001, name: "Karesi" },
     { id: 1002, name: "Altıeylül" },
@@ -12,4 +17,5 @@ export const districts: Record<number, { id: number; name: string }[]> = {
     { id: 3401, name: "Kadıköy" },
     { id: 3402, name: "Üsküdar" },
   ],
+  
 };
